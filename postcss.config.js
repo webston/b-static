7053388601be88ecc08580549e4bb78fdefb8104
Harry Chakryan <harry@webnerds.se>
2020-03-09 12:23:39 +0200

    Structure update

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('postcss-nested'), // or require('postcss-nesting')
-    require('autoprefixer'),
-    require('postcss-object-fit-images')
-  ]
-}
